cde66f9126f35d0501cdece8c4a5a4d91cb44ee3
Nick <nicholasconnors@hotmail.ca>
2022-08-21 18:45:44 -0400

    Add CreateTrigger and SetCurrentRootPresence to api

---
+++ API/RichPresenceAPI.cs
@@ -0,0 +1,24 @@
+﻿using DiscordRPC;
+using System;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputRemoting;
+
+namespace OWRichPresence.API;
+
+public class RichPresenceAPI : IRichPresenceAPI
+{
+    public void SetRichPresence(string message, int imageKey)
+        => OWRichPresence.SetPresence(message, (ImageKey)imageKey);
+
+    public void SetTriggerActivation(bool active)
+        => OWRichPresence.TriggersActive = active;
+
+    public void CreateTrigger(GameObject parent, Sector sector, string message, string imageKey)
+    {
+        var trigger = OWRichPresence.CreateTrigger(parent, message, (ImageKey)Enum.Parse(typeof(ImageKey), imageKey));
+        trigger.SetSector(sector);
+    }
+
+    public void SetCurrentRootPresence(string message, string imageKey) 
+        => OWRichPresence.Instance.SetRootPresence(message, (ImageKey)Enum.Parse(typeof(ImageKey), imageKey));
+}
