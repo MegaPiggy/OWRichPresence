443b5711c647055e5f4535edec9f71e479adec6f
Noah Pilarski <mehmehmeh313@gmail.com>
2022-08-21 19:40:37 -0400

    Move ImageKey to a separate file

---
+++ ImageKey.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWRichPresence
+{
+	public enum ImageKey
+	{
+		// Vanilla
+		ashtwin,
+		attlerock,
+		brittlehollow,
+		darkbramble,
+		dreamworld,
+		embertwin,
+		eyeoftheuniverse,
+		giantsdeep,
+		hollowslantern,
+		hourglasstwins,
+		interloper,
+		orbitalprobecannon,
+		outerwilds,
+		quantummoon,
+		ship,
+		skyshutter,
+		stranger,
+		sun,
+		sunstation,
+		timberhearth,
+		whitehole,
+
+		//New Horizons
+		newhorizons = 96,
+
+		//New Horizons Examples
+		darkgateway = 100,
+		dauntingconfidant,
+		devilsmaw,
+		frigidpygmy,
+		giantsdeepexamples,
+		lavatwin,
+		lavatwins,
+		lightgateway,
+		lunalure,
+		nightlight,
+		ringedjewel,
+		sequesteredluminary,
+		snowball,
+		terralure,
+		wetrock,
+
+		//RSS
+		barnardsstar = 200,
+		barnardsstarb,
+		callisto,
+		ceres,
+		charon,
+		deimos,
+		earth,
+		europa,
+		ganymede,
+		halleyscomet,
+		hydra,
+		io,
+		jupiter,
+		kerberos,
+		luhman16a,
+		luhman16b,
+		mars,
+		mercury,
+		moon,
+		neptune,
+		nix,
+		perdition,
+		phobos,
+		pluto,
+		proteus,
+		proximab,
+		proximac,
+		proximacentauri,
+		puck,
+		rigilkentaurus,
+		saturn,
+		sol,
+		styx,
+		titan,
+		titania,
+		toliman,
+		triton,
+		uranus,
+		venus,
+		vesta
+	}
+}
