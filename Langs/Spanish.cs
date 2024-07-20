ad9bcd59173766fc22fe27be4af6a20cb9372d78
Miguerubsk <miguerubsk@gmail.com>
2024-07-20 14:21:17 +0100

    Create Spanish.cs

---
+++ Langs/Spanish.cs
@@ -0,0 +1,147 @@
+namespace OWRichPresence.Langs
+{
+	public class Spanish
+	{
+		public static string author = "Miguerubsk";
+		public static OWDiscordRPCTranslation Content = new()
+		{
+			TitleScreen = "En la pantalla del título.",
+			SolarSystem = new SolarSystem
+			{
+				Exploring = "Explorando el sistema solar.",
+				TimberHearth = new TimberHearth
+				{
+					Exploring = "Explorando Lumbre.",
+					Sleeping = "Durmiendo bajo las estrellas.",
+					LaunchPad = "En el panel de lanzamiento.",
+					ZeroG = "Explorando la cueva de Gravedad 0.",
+					Tuff = "Visitando a Toba.",
+					Village = "Explorando la aldea",
+					Cemetery = "Visitando el cementerio.",
+					LowerVillage = "Explorando la aldea baja.",
+					UpperVillage = "Explorando la aldea alta.",
+					Mica = "Visitando a Mica.",
+					Gneiss = "Visitando a Gneis.",
+					Spinel = "Visitando a Espinela.",
+					Rutile = "Visitando a Rutilo.",
+					Marl = "Visitando a Marga.",
+					Porphy = "Visitando a Porfi.",
+					Tephra = "Visitando a Tefra.",
+					Galena = "Visitando a Galena.",
+					FoundGalena = "Encontró a Galena.",
+					FoundTephra = "Encontró a Tephra.",
+					Slate = "Visitando a Launa.",
+					Gossan = "Visitando a Gossan.",
+					Tektite = "Visitando a Tectita.",
+					Arkose = "Visitando a Arkosa.",
+					Moraine = "Visitando a Morrena.",
+					Observatory = "Visitando el Observatorio.",
+					Hal = "Visitando a Hali.",
+					Hornfels = "Visitando a Corné.",
+					Inside = "Dentro de Lumbre.",
+					Crater = "Explorando un crater.",
+					YoungbarkCrater = "Explorando el Crater Youngbark.",
+					RadioTower = "Visitando la Torre de Radio.",
+					SkyShutter = "Jugando con el Satélite \"Sky Shutter\"."
+				},
+				Attlerock = new Attlerock
+				{
+					Exploring = "Explorando Rocaterra.",
+					Esker = "Visitando a Esker."
+				},
+				BrittleHollow = new BrittleHollow
+				{
+					Exploring = "Explorando Hondonada Frágil.",
+					Crossroads = "Exploring the Crossroads.",
+					Riebeck = "Visitando a Riebeck."
+				},
+				HollowsLantern = new HollowsLantern
+				{
+					Exploring = "Explorando Farol de Hondonada."
+				},
+				Sun = new Sun
+				{
+					Burning = "Quemandose cerca del Sol.",
+					Orbiting = "Orbitando el Sol."
+				},
+				AshTwin = new AshTwin
+				{
+					Exploring = "Explorando el Gemelo Ceniza."
+				},
+				EmberTwin = new EmberTwin
+				{
+					Exploring = "Explorando el Gemelo Ascua.",
+					Chert = "Visitando a Chert."
+				},
+				QuantumMoon = new QuantumMoon
+				{
+					Exploring = "Explorando un lugar extraño..."
+				},
+				DreamWorld = new DreamWorld
+				{
+					Exploring = "Explorando un lugar extraño..."
+				},
+				Stranger = new Stranger
+				{
+					Exploring = "Explorando un lugar extraño..."
+				},
+				GiantsDeep = new GiantsDeep
+				{
+					Exploring = "Explorando el Abismo del Gigante.",
+					Orbiting = "Orbitando el Abismo del Gigante.",
+					GabbrosShip = "Comprobando la nave de Gabbro.",
+					StatueIsland = "Explorando la Isla de la Estatua.",
+					GabbroIsland = "Explorando la Isla de Gabbro.",
+					Gabbro = "Visitando a Gabbro.",
+					Yard = "Explorando el Sitio de Construcción.",
+					BrambleIsland = "Explorando la Isla Espina.",
+					Somewhere = "Explorando un lugar extraño..."
+				},
+				DarkBramble = new DarkBramble
+				{
+					Exploring = "Explorando Espinoscuro.",
+					Orbiting = "Orbitando Espinoscuro.",
+					Somewhere = "En algún lugar de Espinoscuro..."
+				},
+				WhiteHole = new WhiteHole
+				{
+					Exploring = "Explorando el Agujero Blanco."
+				},
+				WhiteHoleStation = new WhiteHoleStation
+				{
+					Exploring = "Explorando la Estación del Agujero Blanco."
+				},
+				HourglassTwins = new HourglassTwins
+				{
+					Exploring = "Explorando los Gemelos Reloj de Arena."
+				},
+				Interloper = new Interloper
+				{
+					Exploring = "Explorando el Intruso."
+				},
+				MapSatellite = new MapSatellite
+				{
+					Checking = "Comprobando el Mapa Satélite."
+				},
+				BackerSatellite = new BackerSatellite
+				{
+					Checking = "Comprobando el Satélite Backer."
+				},
+				Ship = new Ship
+				{
+					Inside = "Dentro de la nave."
+				}
+			},
+			EyeOfTheUniverse = "En algún lugar...",
+			CreditsFast = "Viendo los créditos.",
+			CreditsFinal = "Terminando el juego.",
+			PostCreditScene = "14.3 billones de años después...",
+			Unknown = "Desconocido",
+			Custom = new Custom
+			{
+				Exploring = "Explorando {0}.",
+				Warping = "Teleportándose a {0}."
+			}
+		};
+	}
+}
