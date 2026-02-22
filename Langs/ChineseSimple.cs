ee474fffd71630051cac288c64548b34c25fe4ea
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-02-22 12:13:16 -0500

    format

--- Langs/ChineseSimple.cs
+++ Langs/ChineseSimple.cs
@@ -1,147 +1,147 @@
 namespace OWRichPresence.Langs
 {
-    public class ChineseSimple
-    {
-        public static string author = "SheepCreater2";
-        public static OWDiscordRPCTranslation Content = new()
-        {
-            TitleScreen = "位于标题页面中",
-            SolarSystem = new SolarSystem
-            {
-                Exploring = "正在探索太阳系",
-                TimberHearth = new TimberHearth
-                {
-                    Exploring = "正在探索木炉星",
-                    Sleeping = "正在星空下睡觉",
-                    LaunchPad = "位于发射平台上",
-                    ZeroG = "正在探索零重力洞窟",
-                    Tuff = "正在拜访塔夫",
-                    Village = "正在探索村庄",
-                    Cemetery = "正在探索墓地",
-                    LowerVillage = "正在探索村庄下半部分",
-                    UpperVillage = "正在探索村庄上半部分",
-                    Mica = "正在拜访米卡",
-                    Gneiss = "正在拜访奈斯",
-                    Spinel = "正在拜访斯宾奈尔",
-                    Rutile = "正在拜访鲁迪尔",
-                    Marl = "正在拜访马尔",
-                    Porphy = "正在拜访波菲",
-                    Tephra = "正在拜访泰弗拉",
-                    Galena = "正在拜访加利纳",
-                    FoundGalena = "找到加利纳",
-                    FoundTephra = "找到泰弗拉",
-                    Slate = "正在拜访斯雷特",
-                    Gossan = "正在拜访古萨",
-                    Tektite = "正在拜访泰克泰特",
-                    Arkose = "正在拜访阿寇斯",
-                    Moraine = "正在拜访莫瑞恩",
-                    Observatory = "正在参观天文台",
-                    Hal = "正在拜访哈尔",
-                    Hornfels = "正在拜访霍恩费斯",
-                    Inside = "正在木炉星内部",
-                    Crater = "正在探索陨石坑",
-                    YoungbarkCrater = "正在探索量子树林",
-                    RadioTower = "正在探索无线电塔",
-                    SkyShutter = "正在检查\"天空快门\" 卫星"
-                },
-                Attlerock = new Attlerock
-                {
-                    Exploring = "正在探索废岩星",
-                    Esker = "正在拜访埃斯科"
-                },
-                BrittleHollow = new BrittleHollow
-                {
-                    Exploring = "正在探索碎空星",
-                    Crossroads = "正在探索十字路口",
-                    Riebeck = "正在拜访瑞拜克"
-                },
-                HollowsLantern = new HollowsLantern
-                {
-                    Exploring = "正在探索空心灯"
-                },
-                Sun = new Sun
-                {
-                    Burning = "正在太阳附近被焚烧",
-                    Orbiting = "正在尝试成为太阳的一个卫星"
-                },
-                AshTwin = new AshTwin
-                {
-                    Exploring = "正在探索余烬双星"
-                },
-                EmberTwin = new EmberTwin
-                {
-                    Exploring = "正在探索余烬双星",
-                    Chert = "正在拜访切特"
-                },
-                QuantumMoon = new QuantumMoon
-                {
-                    Exploring = "正在探索某个很奇怪的地方..."
-                },
-                DreamWorld = new DreamWorld
-                {
-                    Exploring = "正在探索某个很奇怪的地方..."
-                },
-                Stranger = new Stranger
-                {
-                    Exploring = "正在探索某个很奇怪的地方..."
-                },
-                GiantsDeep = new GiantsDeep
-                {
-                    Exploring = "正在探索深巨星",
-                    Orbiting = "正在环绕深巨星",
-                    GabbrosShip = "正在检查加布罗的飞船。",
-                    StatueIsland = "正在探索雕像岛",
-                    GabbroIsland = "正在探索加布罗的岛屿",
-                    Gabbro = "正在拜访加布罗",
-                    Yard = "正在探索建造厂",
-                    BrambleIsland = "正在探索荆棘岛",
-                    Somewhere = "正在探索某个很奇怪的地方..."
-                },
-                DarkBramble = new DarkBramble
-                {
-                    Exploring = "正在探索黑棘星",
-                    Orbiting = "正在环绕黑棘星",
-                    Somewhere = "在黑棘星中的某处..."
-                },
-                WhiteHole = new WhiteHole
-                {
-                    Exploring = "正在探索白洞"
-                },
-                WhiteHoleStation = new WhiteHoleStation
-                {
-                    Exploring = "正在探索白洞站"
-                },
-                HourglassTwins = new HourglassTwins
-                {
-                    Exploring = "正在探索沙漏双星"
-                },
-                Interloper = new Interloper
-                {
-                    Exploring = "正在探索闯入者"
-                },
-                MapSatellite = new MapSatellite
-                {
-                    Checking = "检查深空卫星"
-                },
-                BackerSatellite = new BackerSatellite
-                {
-                    Checking = "检查赞助者卫星"
-                },
-                Ship = new Ship
-                {
-                    Inside = "在飞船里面"
-                }
-            },
-            EyeOfTheUniverse = "在某个地方...",
-            CreditsFast = "正在观看制作人员名单",
-            CreditsFinal = "已通关游戏",
-            PostCreditScene = "143亿年后...",
-            Unknown = "未知",
-            Custom = new Custom
-            {
-                Exploring = "正在探索{0}.",
-                Warping = "正在跃迁到{0}."
-            }
-        };
-    }
+	public class ChineseSimple
+	{
+		public static string author = "SheepCreater2";
+		public static OWDiscordRPCTranslation Content = new()
+		{
+			TitleScreen = "位于标题页面中",
+			SolarSystem = new SolarSystem
+			{
+				Exploring = "正在探索太阳系",
+				TimberHearth = new TimberHearth
+				{
+					Exploring = "正在探索木炉星",
+					Sleeping = "正在星空下睡觉",
+					LaunchPad = "位于发射平台上",
+					ZeroG = "正在探索零重力洞窟",
+					Tuff = "正在拜访塔夫",
+					Village = "正在探索村庄",
+					Cemetery = "正在探索墓地",
+					LowerVillage = "正在探索村庄下半部分",
+					UpperVillage = "正在探索村庄上半部分",
+					Mica = "正在拜访米卡",
+					Gneiss = "正在拜访奈斯",
+					Spinel = "正在拜访斯宾奈尔",
+					Rutile = "正在拜访鲁迪尔",
+					Marl = "正在拜访马尔",
+					Porphy = "正在拜访波菲",
+					Tephra = "正在拜访泰弗拉",
+					Galena = "正在拜访加利纳",
+					FoundGalena = "找到加利纳",
+					FoundTephra = "找到泰弗拉",
+					Slate = "正在拜访斯雷特",
+					Gossan = "正在拜访古萨",
+					Tektite = "正在拜访泰克泰特",
+					Arkose = "正在拜访阿寇斯",
+					Moraine = "正在拜访莫瑞恩",
+					Observatory = "正在参观天文台",
+					Hal = "正在拜访哈尔",
+					Hornfels = "正在拜访霍恩费斯",
+					Inside = "正在木炉星内部",
+					Crater = "正在探索陨石坑",
+					YoungbarkCrater = "正在探索量子树林",
+					RadioTower = "正在探索无线电塔",
+					SkyShutter = "正在检查\"天空快门\" 卫星"
+				},
+				Attlerock = new Attlerock
+				{
+					Exploring = "正在探索废岩星",
+					Esker = "正在拜访埃斯科"
+				},
+				BrittleHollow = new BrittleHollow
+				{
+					Exploring = "正在探索碎空星",
+					Crossroads = "正在探索十字路口",
+					Riebeck = "正在拜访瑞拜克"
+				},
+				HollowsLantern = new HollowsLantern
+				{
+					Exploring = "正在探索空心灯"
+				},
+				Sun = new Sun
+				{
+					Burning = "正在太阳附近被焚烧",
+					Orbiting = "正在尝试成为太阳的一个卫星"
+				},
+				AshTwin = new AshTwin
+				{
+					Exploring = "正在探索余烬双星"
+				},
+				EmberTwin = new EmberTwin
+				{
+					Exploring = "正在探索余烬双星",
+					Chert = "正在拜访切特"
+				},
+				QuantumMoon = new QuantumMoon
+				{
+					Exploring = "正在探索某个很奇怪的地方..."
+				},
+				DreamWorld = new DreamWorld
+				{
+					Exploring = "正在探索某个很奇怪的地方..."
+				},
+				Stranger = new Stranger
+				{
+					Exploring = "正在探索某个很奇怪的地方..."
+				},
+				GiantsDeep = new GiantsDeep
+				{
+					Exploring = "正在探索深巨星",
+					Orbiting = "正在环绕深巨星",
+					GabbrosShip = "正在检查加布罗的飞船。",
+					StatueIsland = "正在探索雕像岛",
+					GabbroIsland = "正在探索加布罗的岛屿",
+					Gabbro = "正在拜访加布罗",
+					Yard = "正在探索建造厂",
+					BrambleIsland = "正在探索荆棘岛",
+					Somewhere = "正在探索某个很奇怪的地方..."
+				},
+				DarkBramble = new DarkBramble
+				{
+					Exploring = "正在探索黑棘星",
+					Orbiting = "正在环绕黑棘星",
+					Somewhere = "在黑棘星中的某处..."
+				},
+				WhiteHole = new WhiteHole
+				{
+					Exploring = "正在探索白洞"
+				},
+				WhiteHoleStation = new WhiteHoleStation
+				{
+					Exploring = "正在探索白洞站"
+				},
+				HourglassTwins = new HourglassTwins
+				{
+					Exploring = "正在探索沙漏双星"
+				},
+				Interloper = new Interloper
+				{
+					Exploring = "正在探索闯入者"
+				},
+				MapSatellite = new MapSatellite
+				{
+					Checking = "检查深空卫星"
+				},
+				BackerSatellite = new BackerSatellite
+				{
+					Checking = "检查赞助者卫星"
+				},
+				Ship = new Ship
+				{
+					Inside = "在飞船里面"
+				}
+			},
+			EyeOfTheUniverse = "在某个地方...",
+			CreditsFast = "正在观看制作人员名单",
+			CreditsFinal = "已通关游戏",
+			PostCreditScene = "143亿年后...",
+			Unknown = "未知",
+			Custom = new Custom
+			{
+				Exploring = "正在探索{0}.",
+				Warping = "正在跃迁到{0}."
+			}
+		};
+	}
 }
